626c721c9dfb38cbd5c055e00a41428df9313fc8
Jonas Rönsch <jonas.roensch@udo.edu>
2020-07-01 17:30:37 +0200

    Auswertung ist bis zum Wasserstoffmodel fertig jej

--- V_23_Quanten_Analogie/auswertungen/auswertung_Peak_aufspaltung.py
+++ V_23_Quanten_Analogie/auswertungen/auswertung_Peak_aufspaltung.py
@@ -15,12 +15,37 @@ peak_two_9mm = Data_9mm[0][160:len(Data_9mm[1]) -1][np.argmax(Data_9mm[1][160:le
 peak_one_12mm = Data_12mm[0][0:160][np.argmax(Data_12mm[1][0:160]) ]
 peak_two_12mm = Data_12mm[0][160:len(Data_12mm[1]) -1][np.argmax(Data_12mm[1][160:len(Data_12mm[1]) -1]) ]
 
+def linear(x,a,b):
+    return(a*x + b)
 
+print(peak_one_3mm)
+print(peak_one_9mm)
+print(peak_one_12mm)
+print()
+print(peak_two_3mm)
+print(peak_two_9mm)
+print(peak_two_12mm)
+print()
 Aufspaltung = [peak_two_3mm - peak_one_3mm, peak_two_9mm - peak_one_9mm, peak_two_12mm - peak_one_12mm]
-
+for i in Aufspaltung:
+    print(i)
 Dicke = [3, 9, 12]
 
+x_plot = np.linspace(3,12,2)
+
+params,covar_matrix = curve_fit(linear, Dicke, Aufspaltung)
+
+error = np.sqrt(np.diag(covar_matrix))
+
+uparams = unp.uarray(params,error)
+
+for i in range(2):
+    print(uparams[i])
+
 plt.figure()
-plt.plot(Dicke, Aufspaltung)
-plt.show()
+plt.plot(Dicke, Aufspaltung,"rx")
+plt.plot(x_plot,linear(x_plot,*params))
+plt.xlabel("d / mm")
+plt.ylabel(r"$\Delta$ f / Hz")
+plt.savefig("../latex-template/figure/Peak_Aufspaltung.pdf")
 plt.close()
