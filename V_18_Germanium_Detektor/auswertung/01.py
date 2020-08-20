a4fbcf4672194bfe5e739a60584a36a116d71507
Jonas Rönsch <jonas.roensch@udo.edu>
2020-08-20 10:57:58 +0200

    V18 geht richtig los

--- V_18_Germanium_Detektor/auswertung/01.py
+++ V_18_Germanium_Detektor/auswertung/01.py
@@ -5,17 +5,46 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 import scipy.constants as const
 from time import time 
+from scipy.signal import find_peaks
+import pandas as pd
 
 start_time = time()
 A = np.genfromtxt("../Data/01.txt", unpack=True)
+P,I,K_plot= np.genfromtxt("../Data/01_peaks.txt", unpack=True)
 
 Kanal = np.arange(0,len(A))
 
 
-plt.figure()
+def linear(x,m,b):
+    return(m*x +b)
+
+
+params,cov_matrix = curve_fit(linear,K_plot,P)
+
+error = np.sqrt(np.diag(cov_matrix))
+uparams = unp.uarray(params, error)
+for i in uparams:
+    print(i)
+
+
+A_plot = []
+for i in K_plot:
+    A_plot = np.append(A_plot,A[Kanal==i])
+
+plt.figure(figsize=(13,8))
 plt.plot(Kanal,A)
+plt.plot(K_plot,A_plot, "rx")
 plt.show()
 plt.close()
+
+plt.figure(figsize=(13,8))
+plt.plot(K_plot,P, "rx")
+plt.plot(K_plot,linear(K_plot,*params))
+plt.show()
+plt.close()
+
+
+
 
 
 lam = np.log(2)/13.516
