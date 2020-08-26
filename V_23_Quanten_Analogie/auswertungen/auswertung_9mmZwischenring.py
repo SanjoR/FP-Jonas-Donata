5de705d804b183774fe1a696fba4b7636f550add
Jonas Rönsch <jonas.roensch@udo.edu>
2020-08-26 09:16:06 +0200

    V23 Korrektur angefangen

--- V_23_Quanten_Analogie/auswertungen/auswertung_9mmZwischenring.py
+++ V_23_Quanten_Analogie/auswertungen/auswertung_9mmZwischenring.py
@@ -19,21 +19,23 @@ def new_sph(x,m,n):
 
 max_winkel = new_sph(Winkel,1,1).max()
 
-Winkel_plot = np.linspace(0,np.pi,200)
+Winkel_plot = np.linspace(0,2*np.pi,200)
 
 
 
 
-for n in range (3):
+for n in range (2):
     ax = plt.subplot(111, projection='polar')
-    ax.plot(Winkel, Amplitude, "r-", lw = 5 ,label = "Messdaten")
+    ax.plot(Winkel, Amplitude, "rx", lw = 5 ,label = "Messdaten")
+    ax.set_title(f"l={n}")
     for m in range(-2,3):
         if abs(m) <= n:
-            ax.plot(Winkel_plot,new_sph(Winkel_plot,m,n)   , label= f"m={m}, n={n}")
+            ax.plot(Winkel_plot,Amplitude.max()*np.absolute(np.real(sph_harm(m,n,np.pi/4,Winkel_plot)))   , label= f"m={m}")
     ax.set_rmax(1)
 #ax.set_rticks([0.5, 1, 1.5, 2])  # less radial ticks
     ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
     ax.grid(True)
     plt.legend(loc = "best")
+    plt.show()
     plt.savefig(f"../latex-template/figure/9mmZwischenring_n{n}.pdf")
     plt.close()
--- V_23_Quanten_Analogie/auswertungen/auswertung_Resonanzen_Drehwinkel.py
+++ V_23_Quanten_Analogie/auswertungen/auswertung_Resonanzen_Drehwinkel.py
@@ -18,13 +18,13 @@ def new_Winkel(x):
 for i in Winkel:
     print(round(180 * new_Winkel(i) / np.pi,1))
 def new_sph(x,m,n):
-    return(sph_harm(m,n,0, x)) 
+    return(sph_harm(m,n,x, 0)) 
 
-Winkel_plot = new_Winkel(np.linspace(0,np.pi,100))
+Winkel_plot = np.linspace(0,2*np.pi,1000)
 
 ax = plt.subplot(111, projection='polar')
-ax.plot(new_Winkel(Winkel), Amplitude_2, "r-", lw= 3, label = "Messwerte")
-ax.plot(new_Winkel(Winkel),Amplitude_2.max()*new_sph(Winkel,0,0)   , label= f"m={0}, n={0}")
+ax.plot(new_Winkel(Winkel), Amplitude_2, "rx", lw= 3, label = "Messwerte")
+ax.plot(Winkel_plot,np.absolute(np.real(Amplitude_2.max()*sph_harm(0,0,Winkel_plot,0)))   , label= f"m={0}, n={0}")
 ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
 ax.grid(True)
 plt.legend(loc= "best")
@@ -37,26 +37,28 @@ plt.close()
 
 for n in range (1,5):
     ax = plt.subplot(111, projection='polar')
-    ax.plot(new_Winkel(Winkel), Amplitude_4, "r-", lw= 3,label = "Messwerte")
-    ax.plot(Winkel_plot,Amplitude_4.max()*new_sph(Winkel_plot,0,n),"b", label= f"n={n}")
+    ax.plot(new_Winkel(Winkel), Amplitude_4, "rx", lw= 3,label = "Messwerte")
+    ax.plot(Winkel_plot,np.absolute(np.real(Amplitude_4.max()*sph_harm(0,n,0,Winkel_plot))),"b", label= f"l={n} m=0")
 #ax.set_rmax(2)
 #ax.set_rticks([0.5, 1, 1.5, 2])  # less radial ticks
     ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
     ax.grid(True)
     plt.legend(loc= "best")
     plt.savefig(f"../latex-template/figure/Resonanz_Drewinkel_Amplitude_4_n{n}.pdf")
+    plt.show()
     plt.close()
 
 
 
-for n in range (4,8):
+for n in range (1,4):
     ax = plt.subplot(111, projection='polar')
-    ax.plot(new_Winkel(Winkel), Amplitude_6, "r-", lw= 3,label = "Messwerte")
-    ax.plot(Winkel_plot,Amplitude_6.max()*new_sph(Winkel_plot,0,n) ,"b-"  , label= f"n={n}")
+    ax.plot(new_Winkel(Winkel), Amplitude_6, "rx", lw= 3,label = "Messwerte")
+    ax.plot(Winkel_plot,np.absolute(np.real(Amplitude_6.max()*sph_harm(0,n,0,Winkel_plot))) ,"b-"  , label= f"l={n} m=0")
     ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
     ax.grid(True)
     plt.legend(loc= "best")
     plt.savefig(f"../latex-template/figure/Resonanz_Drewinkel_Amplitude_6_n{n}.pdf")
+    plt.show()
     plt.close()
 #ax.set_rmax(2)
 #ax.set_rticks([0.5, 1, 1.5, 2])  # less radial ticks
